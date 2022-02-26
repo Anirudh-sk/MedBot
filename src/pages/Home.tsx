0978853bf9e4d51e7d5727d6eee1f892c63c2cf5
ani <amazinganirudhhere@gmail.com>
2022-02-26 11:11:37 +0530

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,7 @@ const Home: React.FC = () => {
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
+          <IonTitle>Med Bot</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -44,7 +44,7 @@ const Home: React.FC = () => {
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">
-              Inbox
+              Med Bot
             </IonTitle>
           </IonToolbar>
         </IonHeader>
